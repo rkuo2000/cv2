f4984df358b7a548913fdf89d3b52a671e934a3c
Richard Kuo <rkuo2000@gmail.com>
2017-10-31 19:36:09 +0800

    Add files via upload

---
+++ cam_eye_tracking.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_righteye_2splits.xml')
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    eyes = eye_cascade.detectMultiScale(gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        roi_gray2 = gray[ey:ey+eh, ex:ex+ew]
+        roi_color2 = img[ey:ey+eh, ex:ex+ew]
+        circles = cv2.HoughCircles(roi_gray2,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+        try:
+            for i in circles[0,:]:
+                # draw the outer circle
+                cv2.circle(roi_color2,(i[0],i[1]),i[2],(255,255,255),2)
+                # draw the center of the circle
+                cv2.circle(roi_color2,(i[0],i[1]),2,(255,255,255),3)
+        except Exception as e:
+            print('Error')
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+		
+cap.release()
+cv2.destroyAllWindows()
---
+++ jpg_hough_circles.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('opencv-logo.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img = cv2.medianBlur(gray,5)
+cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+cv2.imshow('detected circles',cimg)
+cv2.waitKey(0)
+
+cv2.release()
+cv2.destroyAllWindows()
