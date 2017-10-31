9591768bf8210801a846d29f50ea7b6dd0752307
Richard Kuo <rkuo2000@gmail.com>
2017-10-31 22:06:42 +0800

    Add files via upload

--- cam_eye_tracking.py
+++ cam_eye_tracking.py
@@ -1,30 +1,22 @@
-import numpy as np
+import face_recognition
 import cv2
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_righteye_2splits.xml')
 
 cap = cv2.VideoCapture(0)
 
 while 1:
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    eyes = eye_cascade.detectMultiScale(gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-        roi_gray2 = gray[ey:ey+eh, ex:ex+ew]
-        roi_color2 = img[ey:ey+eh, ex:ex+ew]
-        circles = cv2.HoughCircles(roi_gray2,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
-        try:
-            for i in circles[0,:]:
-                # draw the outer circle
-                cv2.circle(roi_color2,(i[0],i[1]),i[2],(255,255,255),2)
-                # draw the center of the circle
-                cv2.circle(roi_color2,(i[0],i[1]),2,(255,255,255),3)
-        except Exception as e:
-            print('Error')
-    cv2.imshow('img',img)
+    ret, image = cap.read()
+
+    # Find all facial features in all the faces in the image
+    face_landmarks_list = face_recognition.face_landmarks(image)
+    #print("I found {} face(s) in thisc photograph.".format(len(face_landmarks_list)))
+
+    for face_landmarks in face_landmarks_list:
+        for dot in face_landmarks['left_eye']:
+            cv2.line(image, dot, dot, (0,255,0), 1)
+        for dot in face_landmarks['right_eye']:
+            cv2.line(image, dot, dot, (0,255,0), 1)			
+        cv2.imshow('FACIAL LANDMARK', image)
+
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
