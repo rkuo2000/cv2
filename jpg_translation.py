2035ed7324e229cf2282c892d45557db6c598797
Richard Kuo <rkuo2000@gmail.com>
2018-04-28 12:03:47 +0800

    Update jpg_translation.py

--- jpg_translation.py
+++ jpg_translation.py
@@ -1,7 +1,8 @@
+# openCV read test.jpg and run warpAffine to transform the image
 import cv2
 import numpy as np
 
-img = cv2.imread('c:/test.jpg')
+img = cv2.imread('test.jpg')
 rows,cols = img.shape[:2]
 M = np.float32([[1,0,100],[0,1,50]])
 dst = cv2.warpAffine(img, M, (cols, rows))
