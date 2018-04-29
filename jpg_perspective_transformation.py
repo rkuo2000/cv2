a7be361f1fefe2576815f74a624949c96b081725
Richard Kuo <rkuo2000@gmail.com>
2018-04-29 20:15:13 +0800

    Add files via upload

---
+++ jpg_perspective_transformation.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread('test.jpg') #270x178 pixels
+rows,cols,ch = img.shape
+
+#pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
+pts1 = np.float32([[56,65],[260,52],[28,160],[270,170]])
+pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+
+M = cv2.getPerspectiveTransform(pts1,pts2)
+
+dst = cv2.warpPerspective(img,M,(300,300))
+
+plt.subplot(121),plt.imshow(img),plt.title('Input')
+plt.subplot(122),plt.imshow(dst),plt.title('Output')
+plt.show()
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
