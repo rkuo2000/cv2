f9dc5725c825feeab4a392572f8ec86ffb8051b3
rkuo2000 <rkuo2000@gmail.com>
2018-06-24 19:20:20 +0800

    modified:   jpg_translation.py

--- jpg_translation.py
+++ jpg_translation.py
@@ -2,7 +2,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('test.jpg')
+img = cv2.imread('messi5.jpg')
 rows,cols = img.shape[:2]
 M = np.float32([[1,0,100],[0,1,50]])
 dst = cv2.warpAffine(img, M, (cols, rows))
