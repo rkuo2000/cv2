f80af7924a12969b0cee1e70071cf4c1b20e8d0a
rkuo2000 <rkuo2000@gmail.com>
2018-06-28 03:21:10 +0800

    Changes to be committed: 	modified:   jpg_canny.py

--- jpg_canny.py
+++ jpg_canny.py
@@ -5,6 +5,7 @@ image = cv2.imread('messi5.jpg')
 
 gray  = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 edges = cv2.Canny(gray,100,200)
+cv2.imshow('Gray', gray)
 cv2.imshow('Edges', edges)
 
 cv2.waitKey(0)
