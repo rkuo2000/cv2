e66998a7d2ebb43a76e2f65dcaf1cc5b5c8bd54a
rkuo2000 <rkuo2000@gmail.com>
2018-06-28 03:03:22 +0800

    Changes to be committed: 	new file:   jpg_contours.py 	new file:   jpg_contours_AreaRect.py 	new file:   jpg_contours_EnclosingCircle.py 	new file:   jpg_contours_boundingRect.py 	new file:   jpg_contours_fitEllipse.py 	modified:   test.jpg
     Changes not staged for commit:
    	deleted:    cam_guesture_recognition.py
    	deleted:    jpg_contour.py

---
+++ jpg_contours_fitEllipse.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('test.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (11,11), 0)
+edge = cv2.Canny(blur, 20, 160)
+_, cnts, hierarchy = cv2.findContours(edge,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+# draw Enclosing Circle
+for c in cnts:
+    ellipse = cv2.fitEllipse(c)
+    cv2.ellipse(img, ellipse, (0,255,0), 1)
+
+cv2.imshow("contours", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
