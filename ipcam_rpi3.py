4a77925a3e12353b8c42423350cd5af55d1c8e2f
Richard Kuo <rkuo2000@gmail.com>
2018-07-04 09:37:51 +0800

    Add files via upload

---
+++ ipcam_rpi3.py
@@ -0,0 +1,21 @@
+# capture image from RPi3 usb web (running motion), and display using CV2 on PC
+from PIL import Image
+import urllib.request
+import io
+import numpy as np
+import cv2
+
+URL = 'http://192.168.43.176:8080/?action=snapshot -O rpi3_snapshot.jpg'
+
+while 1:
+    with urllib.request.urlopen(URL) as url:
+        f=io.BytesIO(url.read())
+    image = Image.open(f)
+#    image.show()
+
+    img = np.array(image)
+    frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.imshow('CAM', frame)
+    cv2.waitKey(1)
+
+cv2.destroyAllWindows()
