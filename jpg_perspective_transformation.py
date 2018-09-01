770d21e130326dece5b0e129c458445a20011a18
Richard Kuo <rkuo2000@gmail.com>
2018-09-02 04:07:29 +0800

    Add files via upload

--- jpg_perspective_transformation.py
+++ jpg_perspective_transformation.py
@@ -2,11 +2,10 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('test.jpg') #270x178 pixels
+img = cv2.imread('sudokusmall.png')
 rows,cols,ch = img.shape
 
-#pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
-pts1 = np.float32([[56,65],[260,52],[28,160],[270,170]])
+pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
 pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
 
 M = cv2.getPerspectiveTransform(pts1,pts2)
