603e9a523fc0c39cf573d94092d02f27bf227f68
Richard Kuo <rkuo2000@gmail.com>
2019-01-23 20:30:11 +0800

    Add files via upload

--- cam_contour.py
+++ cam_contour.py
@@ -2,32 +2,36 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
-while 1:
-    ret, img = cap.read()
-  
-    height, width, channels = img.shape
-    roi = img
-    #roi = img[int(height*0.7):height, int(width*0.1):int(width*0.9)]
-    #cv2.imshow('ROI', roi)
 
+while(1):
+    # Take each frame
+    _, frame = cap.read()
+
+    height,width,channels = frame.shape
+    roi = frame
+    #roi = frame[int(height*0.7):height, int(width*0.1):int(width*0.9)]
+    cv2.imshow('ROI', roi)
+	
     gray = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (7,7), 0)
-    edge = cv2.Canny(blur, 10, 160)
-    #cv2.imshow('BLUR', blur)
+    edge = cv2.Canny(blur, 20, 160)    
     cv2.imshow('EDGE', edge)
-
-    _, cnts, hierarchy = cv2.findContours(edge,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-    # draw Bounding Rectangle
-    for c in cnts:
-        (x,y,w,h) = cv2.boundingRect(c)
-        if (w*h>10000 and w*h<40000):
-            print(x,y,w,h)
-            cv2.rectangle(roi, (x,y), (x+w, y+h), (0,255,0), 1)
-
-    cv2.imshow('contours', roi)
-
-    cv2.waitKey(1)
 	
-cap.release()
+    _, cnts, hierarchy = cv2.findContours(edge,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+	
+    for c in cnts:
+        (x,y,w,h) = cv2.boundingRect(c)        
+        area=w*h
+        if(area>20000 ):
+            continue
+        cv2.rectangle(roi, (x,y), (x+w, y+h), (0,255,0), 3)
+        cv2.circle(roi, (int(x+w/2),int(y+h/2)), 5, (0,0,255), 5)
+        print("x",x+w/2,"y",y+h/2)      
+        cv2.imshow('contours', roi)  
+	
+  
+    k = cv2.waitKey(5) & 0xFF
+    if k==27:
+        break
+	
 cv2.destroyAllWindows()
