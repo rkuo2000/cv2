222a45043d321b4cadf35113f249ba0e62bfe540
Richard Kuo <rkuo2000@gmail.com>
2019-03-02 01:06:13 +0800

    Update cam_motion_detection.py

--- cam_motion_detection.py
+++ cam_motion_detection.py
@@ -61,7 +61,7 @@ while True:
 	thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255,
 		cv2.THRESH_BINARY)[1]
 	thresh = cv2.dilate(thresh, None, iterations=2)
-	(_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	cnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 	# loop over the contours
 	for c in cnts:
