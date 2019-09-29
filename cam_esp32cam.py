8ab1c98fed8b6416560f9d79db528f2f15c84ddc
Richard Kuo <rkuo2000@gmail.com>
2019-09-29 23:47:04 +0800

    Add files via upload

---
+++ cam_esp32cam.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+from urllib.request import urlopen
+
+url = 'http://192.168.1.5:81/stream'
+CAMERA_BUFFER_SIZE = 4096
+stream = urlopen(url)
+bbb=b''
+
+while True:
+    bbb += stream.read(CAMERA_BUFFER_SIZE)
+    a = bbb.find(b'\xff\xd8')
+    b = bbb.find(b'\xff\xd9')
+    if a>-1 and b>-1:
+        jpg = bbb[a:b+2]
+        bbb = bbb[b+2:]
+        img = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8),cv2.IMREAD_COLOR)
+        cv2.imshow('CAM', img)
+    cv2.waitKey(1)
+
+cv2.destroyAllWindows()
