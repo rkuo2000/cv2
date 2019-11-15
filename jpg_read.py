7519eb45b2f3d41973074939704e5cb79c50bf14
Richard Kuo <rkuo2000@gmail.com>
2019-11-16 01:57:34 +0800

    Add files via upload

--- jpg_read.py
+++ jpg_read.py
@@ -1,6 +1,9 @@
-# openCV read test.jpg and display the image file
 import cv2
+import sys
 
-img = cv2.imread('test.jpg')
-cv2.imshow('TEST',img)
+if len(sys.argv)>1:
+    img = cv2.imread(sys.argv[1])
+else:
+    img = cv2.imread('test.jpg')
+cv2.imshow('Image',img)
 cv2.waitKey(0)
