e7d216f8368ac2557db6e430eeb8e92fbc6be33b
Richard Kuo <rkuo2000@gmail.com>
2019-11-23 07:29:28 +0800

    Add files via upload

--- face_detect.py
+++ face_detect.py
@@ -1,24 +1,22 @@
-# Usage: python face_detect.py 1.jpg
+# Usage: python face_detect.py friends.jpg
 import numpy as np
 import cv2
 import sys
 
 filename = sys.argv[1]
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
 img  = cv2.imread(filename)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
+face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+bboxes = face.detectMultiScale(gray)
+
+for box in bboxes:
+    print(box)
+
+for (x,y,w,h) in bboxes:
     img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
 cv2.imshow('img',img)
 cv2.waitKey(0)
