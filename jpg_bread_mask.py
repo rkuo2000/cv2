8ee6a583d618f244cd3303684caa8b06296400bd
Richard Kuo <rkuo2000@gmail.com>
2019-12-30 05:14:29 +0800

    Add files via upload

--- jpg_bread_mask.py
+++ jpg_bread_mask.py
@@ -1,38 +1,51 @@
 import numpy as np
 import cv2
 
-for i in range(10):
-DATA_PATH= './bread/croissant'
-FILENAME = 'images-0.jpg'
-file = DATA_PATH+'/'+FILENAME
+DIR_PATH   = './bread'
+BREAD_NAME = 'croissant'
+BREAD_PATH= './bread/'+BREAD_NAME
 
-img = cv2.imread(file)
-height, width, channels = img.shape
+for i in range(17,25):
+    file = BREAD_PATH+'-'+str(i)
+    print(file+'.jpg')	
+    img = cv2.imread(file+'.jpg')
 
-#gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#hsv  = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    # Range of Color
+    lower_white = np.array([200,200,200])
+    upper_white = np.array([255,255,255])
 
-# Range of Color
-lower_white = np.array([200,200,200])
-upper_white = np.array([255,255,255])
+    mask = cv2.inRange(img, lower_white, upper_white)
+    mask = cv2.bitwise_not(mask)
 
-mask = cv2.inRange(img, lower_white, upper_white)
-mask = cv2.bitwise_not(mask)
+    x, y, w, h = cv2.boundingRect(mask)
+    #bbox = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
+    #cv2.imshow('image', img)
 
-x, y, w, h = cv2.boundingRect(mask)
-#bbox = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
-#cv2.imshow('image', img)
+    img = img[y:y+h, x:x+w]
+    mask = cv2.inRange(img, lower_white, upper_white)
+    mask = cv2.bitwise_not(mask)
+    cv2.imshow('mask', mask)
+    cv2.imwrite(file+'-mask.jpg', mask)
 
-img = img[y:y+h, x:x+w]
-mask = cv2.inRange(img, lower_white, upper_white)
-mask = cv2.bitwise_not(mask)
-cv2.imshow('mask', mask)
-cv2.imwrite(DATA_PATH+'/croissant-0-mask.jpg', mask)
+    res = cv2.bitwise_and(img, img, mask=mask)
+    cv2.imshow('result', res)
+    cv2.imwrite(file+'.jpg', res)
 
-res = cv2.bitwise_and(img, img, mask=mask)
-cv2.imshow('result', res)
-cv2.imwrite(DATA_PATH+'/croissant-0.jpg', res)
-
+    with open(BREAD_PATH+str(i)+'.xml', "w") as text_file:
+        text_file.write("            <object>\n")	
+        text_file.write("                    <name>croissant%s</name>\n" % str(i))
+        text_file.write("                    <pose>Frontal</pose>\n")	
+        text_file.write("                    <truncated>0</truncated>\n")	
+        text_file.write("                    <difficult>0</difficult>\n")	
+        text_file.write("                    <occluded>0</occluded>\n")
+        text_file.write("                    <bndbox>\n")
+        text_file.write("                            <xmin>%s</xmin>\n" % str(x))
+        text_file.write("                            <xmax>%s</xmax>\n" % str(x+w))
+        text_file.write("                            <ymin>%s</ymin>\n" % str(y))
+        text_file.write("                            <ymax>%s</ymax>\n" % str(y+h))	
+        text_file.write("                    </bndbox>\n")
+        text_file.write("            </object>\n")
+	
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
