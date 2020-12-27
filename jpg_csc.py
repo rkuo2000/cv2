9d903ed9a941b5af7b91deb5f442c3f1c952f8c7
Richard Kuo <rkuo2000@gmail.com>
2020-12-28 06:38:13 +0800

    Add files via upload

--- jpg_csc.py
+++ jpg_csc.py
@@ -3,12 +3,15 @@ import cv2
 
 img = cv2.imread('test.jpg')
 gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+org1= cv2.cvtColor(gray,cv2.COLOR_GRAY2BGR)
+org2= cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
-print(type(gray))
-print(gray.shape)
-print(gray)
 cv2.imshow('ORG' ,img)
 cv2.imshow('GRAY',gray)
+cv2.imshow('HSV' ,hsv)
+cv2.imshow('ORG1',org1)
+cv2.imshow('ORG2',org2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
