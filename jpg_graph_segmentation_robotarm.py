38dbe183c287d1645bf7b7fcaa6615bf637edd1b
Richard Kuo <rkuo2000@gmail.com>
2021-03-13 00:17:21 +0800

    Add files via upload

--- jpg_graph_segmentation_robotarm.py
+++ jpg_graph_segmentation_robotarm.py
@@ -3,20 +3,14 @@ import random
 import cv2
 import sys
 
-X1 = int(sys.argv[3])
-Y1 = int(sys.argv[4])
-X2 = int(sys.argv[5])
-Y2 = int(sys.argv[6])
-
 minSize= int(sys.argv[2])
 segmentator = cv2.ximgproc.segmentation.createGraphSegmentation(sigma=0.5, k=300, min_size=minSize)
 
 img = cv2.imread(sys.argv[1])
 height, width, channels = img.shape
 
-roi = img[Y1:Y2, X1:X2]
-segment = segmentator.processImage(roi)
-seg_image = np.zeros(roi.shape, np.uint8)
+segment = segmentator.processImage(img)
+seg_image = np.zeros(img.shape, np.uint8)
 
 for i in range(np.max(segment)):
     y, x = np.where(segment == i)
@@ -24,11 +18,10 @@ for i in range(np.max(segment)):
     for xi, yi in zip(x,y):
         seg_image[yi,xi] = color
 
-result = cv2.addWeighted(roi, 0.3, seg_image, 0.7, 0)
-img[Y1:Y2, X1:X2] = result
+result = cv2.addWeighted(img, 0.3, seg_image, 0.7, 0)
 
-cv2.imshow('img', img)
-cv2.imwrite(sys.argv[1].replace('.jpg', '')+'.out.jpg', img)
+cv2.imshow('result', result)
+cv2.imwrite(sys.argv[1].replace('.cut.jpg', '')+'.out.jpg', result)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
