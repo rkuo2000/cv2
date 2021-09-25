d523590c9a66be8fe5acc1c3b9b8134a54f9a5c2
Richard Kuo <rkuo2000@gmail.com>
2021-09-25 16:45:43 +0800

    Add files via upload

---
+++ optical-flow-dense.py
@@ -0,0 +1,58 @@
+import sys
+import cv2
+import numpy as np
+
+if len(sys.argv)>1:
+    if sys.argv[1]=='cam':
+        mode='camera'
+    else:
+        mode='video'
+    filename = sys.argv[1]
+else:
+    mode='video'
+    filename = 'shibuya.mp4'
+
+if mode=='video':
+    cap = cv2.VideoCapture(filename)
+if mode=='camera':
+    cap = cv2.VideoCapture(0) 
+	
+# ret = a boolean return value from getting the frame, first_frame = the first frame in the entire video sequence
+ret, first_frame = cap.read()
+first_frame = cv2.flip(first_frame, 1) # horizontal flip for mirror effect
+# Converts frame to grayscale because we only need the luminance channel for detecting edges - less computationally expensive
+prev_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
+# Creates an image filled with zero intensities with the same dimensions as the frame
+mask = np.zeros_like(first_frame)
+# Sets image saturation to maximum
+mask[..., 1] = 255
+
+while(cap.isOpened()):
+    # ret = a boolean return value from getting the frame, frame = the current frame being projected in the video
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1) # horizontal flip for mirror effect
+    # Opens a new window and displays the input frame
+    #cv.imshow("input", frame)
+    # Converts each frame to grayscale - we previously only converted the first frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Calculates dense optical flow by Farneback method
+    # https://docs.opencv.org/3.0-beta/modules/video/doc/motion_analysis_and_object_tracking.html#calcopticalflowfarneback
+    flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    # Computes the magnitude and angle of the 2D vectors
+    magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+    # Sets image hue according to the optical flow direction
+    mask[..., 0] = angle * 180 / np.pi / 2
+    # Sets image value according to the optical flow magnitude (normalized)
+    mask[..., 2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
+    # Converts HSV to RGB (BGR) color representation
+    rgb = cv2.cvtColor(mask, cv2.COLOR_HSV2BGR)
+    # Opens a new window and displays the output frame
+    cv2.imshow("dense optical flow", rgb)
+    # Updates previous frame
+    prev_gray = gray
+    # Frames are read by intervals of 1 millisecond. The programs breaks out of the while loop when the user presses the 'q' key
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+# The following frees up resources and closes all windows
+cap.release()
+cv2.destroyAllWindows()
