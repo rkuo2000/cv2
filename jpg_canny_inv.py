d6cedb435480f6c58f6c2990f19b6f40b17bd1af
Richard Kuo <rkuo2000@gmail.com>
2021-11-14 01:54:43 +0800

    Add files via upload

---
+++ jpg_canny_inv.py
@@ -0,0 +1,21 @@
+# openCV read test.jpg, color converted to gray, then run OpenCV canny to generate edge detection
+import cv2
+import sys
+
+if len(sys.argv)>1:
+    filename = sys.argv[1]
+else:
+    filename = 'messi5.jpg'
+
+image = cv2.imread(filename)
+
+gray  = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+edges = cv2.Canny(gray,100,200)
+edges = cv2.bitwise_not(edges)
+cv2.imshow('Gray', gray)
+cv2.imshow('Edges', edges)
+cv2.imwrite('inv_'+filename, edges)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.waitKey(1)
